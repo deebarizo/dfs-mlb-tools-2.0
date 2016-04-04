3ac67f95c83ac234d96945767f8cbb6075c2eab4
Dee Barizo <deebarizo@gmail.com>
2016-04-04 13:25:42 +0000

    Creates helper to convert accent letters to English

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -5,6 +5,7 @@ PRINT VARIABLE
 ****************************************************************************************/
 
 function ddAll($var) {
+
 	echo '<pre>';
 	print_r($var);
 	echo '</pre>';
@@ -13,6 +14,7 @@ function ddAll($var) {
 }
 
 function prf($var) {
+
     echo '<pre>';
     print_r($var);
     echo '</pre>';
@@ -37,5 +39,17 @@ function setTodayDate() {
 	date_default_timezone_set('America/Chicago'); 
 	
 	return date('Y-m-d');	
+}
+
+
+/****************************************************************************************
+CONVERT ACCENT LETTERS TO ENGLISH
+****************************************************************************************/
+
+function convertAccentLettersToEnglish($string) {
+
+	// http://stackoverflow.com/questions/158241/php-replace-umlauts-with-closest-7-bit-ascii-equivalent-in-an-utf-8-string/158265#158265
+
+	return iconv("utf-8", "ascii//TRANSLIT", $string); 
 }
 	
