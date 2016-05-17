01f437e6048e78342f6daa4518d4082620e9c547
Dee Barizo <deebarizo@gmail.com>
2016-05-17 21:55:45 +0000

    Adds filters to player pool view

--- public/js/player_pools/index.js
+++ public/js/player_pools/index.js
@@ -16,6 +16,25 @@ $(document).ready(function() {
 
 		// https://datatables.net/reference/api/column().search()
 
+		if (this.type === 'salary') {
+
+			var salary = Number(this.value);
+
+			if (this.modifier === 'greater-than') {
+
+				console.log(this);
+				console.log(salary);
+
+				playerPoolTable.column(this.columnIndex).data().filter(function(value, salary) {
+
+					return value > salary ? true : false; 
+				}); 
+
+			} else if (this.modifier === 'less-than' ){
+
+			}
+		}
+
 		if (this.type === 'position') {
 
 			if (this.value !== 'Hitters' && this.value !== 'SP' && this.value !== 'All') {
@@ -51,6 +70,67 @@ $(document).ready(function() {
 
 		playerPoolTable.draw();
 	};
+
+
+	/****************************************************************************************
+	SALARY FILTER
+	****************************************************************************************/
+
+	// https://www.datatables.net/examples/plug-ins/range_filtering.html
+
+	$.fn.dataTable.ext.search.push(
+
+	    function(settings, data, dataIndex) {
+	        
+	        var targetSalary = parseInt($('.salary-input').val(), 10);
+
+	        var modifier = $('input:radio[name=salary-toggle]:checked').val();
+
+	        var salary = parseFloat(data[10]) || 0; // use data for the salary column
+
+	        if (modifier === 'greater-than') {
+
+	        	if (salary >= targetSalary) {
+
+	        		return true;
+	        	
+	        	} else {
+
+	        		return false;
+	        	}
+	        }
+
+	        if (modifier === 'less-than') {
+
+	        	if (salary <= targetSalary) {
+
+	        		return true;
+	        	
+	        	} else {
+
+	        		return false;
+	        	}
+	        }		 
+	    }
+	);
+
+	$('.salary-input').keyup(function() {
+
+		playerPoolTable.draw();
+	});
+
+	$("input[name=salary-toggle]:radio").change(function() {
+
+		playerPoolTable.draw();
+	});
+
+
+	$('.salary-reset').on('click', function(event) { 
+		$('.salary-input').val(100000);
+		$('#less-than').prop('checked', true);
+
+		playerPoolTable.draw();
+	});
 
 
 	/****************************************************************************************
